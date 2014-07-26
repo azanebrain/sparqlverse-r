64fcf4184b2faa0ada5e2feb5de90d853d8b8e2f
AJ Zane <aj@sparqlcity.com>
2014-07-26 13:19:32 -0700

    add plotting to svxplayground example

--- svxplayground/ui.R
+++ svxplayground/ui.R
@@ -9,23 +9,27 @@ shinyUI(pageWithSidebar(
   # Slider to limit number of results returned
   sidebarPanel(
     wellPanel(
-    sliderInput("limit",
-                "Limit rows returned:",
-                min = 1,
-                max = 50,
-                value = 10, animate=TRUE)),
-    checkboxInput(inputId = "showQuery",
-                   label = "Show Query",
-                   value = FALSE),
-    conditionalPanel("input.showQuery == true",
-                    p(strong("SPARQL Query")),
-                    textOutput("SPARQLquery")
-                   )),
+      sliderInput("limit",
+                  "Set Your Range:",
+                  min = 1,
+                  max = 500,
+                  step = 10,
+                  value = c(10,500),
+                  animate=TRUE
+                  )
+      ),
+      checkboxInput(inputId = "showQuery",
+                     label = "Show Query",
+                     value = FALSE),
+      conditionalPanel("input.showQuery == true",
+                      p(strong("SPARQL Query")),
+                      textOutput("SPARQLquery")
+                     )),
 
-  # Show the pie chart of most popular states
+  # Show the visualizations of the results
   mainPanel(
     tabsetPanel(
-      tabPanel("Plot", plotOutput("mostPopularStateLine")),
+      tabPanel("Plot", plotOutput("resultsPlot")),
       tabPanel("Table", dataTableOutput("resultsTable"))
     )
   )))
